6df7e2d6c501883923e56165ae36c803b8c2f1c7
Baptiste Angot <bat76640@gmail.com>
2022-04-07 11:39:04 +0200

    init push

--- Program.cs
+++ Program.cs
@@ -9,32 +9,32 @@ namespace csharp
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
+            IList<IItem> Items = new List<IItem>{
+                new AdapterItem {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new AdapterItem {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new AdapterItem {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new AdapterItem {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new AdapterItem {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new AdapterItem
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 15,
                     Quality = 20
                 },
-                new Item
+                new AdapterItem
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 10,
                     Quality = 49
                 },
-                new Item
+                new AdapterItem
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 5,
                     Quality = 49
                 },
 				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+				new AdapterItem {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
 
             var app = new GildedRose(Items);
